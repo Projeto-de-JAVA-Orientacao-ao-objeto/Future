1d8f45b3c633e8fbaa2bd26cb81d2ce6ce859ce1
Gustavo <gustavolopes159@hotmail.com>
2024-11-09 10:58:58 -0300

    feat: primeir commit

--- src/main/java/future/project/future/FutureApplication.java
+++ src/main/java/future/project/future/FutureApplication.java
@@ -8,6 +8,8 @@ public class FutureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FutureApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
+
--- src/test/java/future/project/future/FutureApplicationTests.java
+++ src/test/java/future/project/future/FutureApplicationTests.java
@@ -1,9 +1,10 @@
 package future.project.future;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootApplication
 class FutureApplicationTests {
 
 	@Test
