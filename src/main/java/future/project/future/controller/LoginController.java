c72e3185266169f9c5461662c27fb18438eb2bf7
Gabriel Rezende <gabrielhnrezende@gmail.com>
2024-11-09 16:33:55 -0300

    Para teste

--- src/main/java/future/project/future/controller/LoginController.java
+++ src/main/java/future/project/future/controller/LoginController.java
@@ -1,13 +1,7 @@
 package future.project.future.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import future.project.future.model.Usuario;
-import jakarta.validation.Valid;
 
 
 
@@ -15,25 +9,25 @@ import jakarta.validation.Valid;
 @Controller
 public class LoginController {
     
-    private Usuario ur;
+    // private Usuario ur;
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
-    @GetMapping("/cadastroUsuario") //Essa parte aqui vai ser so para adm dentro da pagina
-    public String cadastro() {
-        return "cadastro";
-    }
+    // @GetMapping("/cadastroUsuario") //Essa parte aqui vai ser so para adm dentro da pagina
+    // public String cadastro() {
+    //     return "cadastro";
+    // }
 
-    @RequestMapping(value="/cadastroUsuario", method=RequestMethod.GET)
-    public String cadastroUsuario(@Valid Usuario usuario, BindingResult result) {
+    // @RequestMapping(value="/cadastroUsuario", method=RequestMethod.GET)
+    // public String cadastroUsuario(@Valid Usuario usuario, BindingResult result) {
 
-        if(result.hasErrors()){
-            return "redirect:/cadastroUsuario";
-        }
-        return "";
-    }
+    //     if(result.hasErrors()){
+    //         return "redirect:/cadastroUsuario";
+    //     }
+    //     return "";
+    // }
     
 }
