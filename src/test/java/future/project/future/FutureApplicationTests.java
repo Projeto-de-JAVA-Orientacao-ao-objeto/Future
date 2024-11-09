1ff98f5d9202e2bd107e2bef311dabcade2554e2
Gabriel Rezende <gabrielhnrezende@gmail.com>
2024-11-09 10:48:17 -0300

    Primeiro commit

---
+++ src/test/java/future/project/future/FutureApplicationTests.java
@@ -0,0 +1,13 @@
+package future.project.future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
